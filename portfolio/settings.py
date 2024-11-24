7cab62564ccb39280452468609588e6acb2c4dc3
Arpan Bera <me.arpan122@gmail.com>
2024-11-25 01:56:28 +0530

    npm  config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
